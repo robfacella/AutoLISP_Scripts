ea3b87ef3320ac45bb45558dedbde7391a0a4ffe
robfacella <rob.facella@gmail.com>
2025-08-06 21:11:46 -0400

    Countdown from X

--- Countdown_X.ps1
+++ Countdown_X.ps1
@@ -1,10 +1,11 @@
-$totalSeconds = 10
-$currentCount = $totalSeconds
+$totalSeconds = $($args[0])	#Set time to wait from command line args
+$currentCount = $totalSeconds	#Countdown from, preserving $totalSenconds args if needed later.
 
 while ($currentCount -gt 0){
     Write-Host "$currentCount seconds remaining..."
-    Start-Sleep -Seconds 1
-    $currentCount--
-}
+    Start-Sleep -Seconds 1	#Sleep for 1 second
+    $currentCount--		#decrease counter by 1
+}				#Do While greater than 0
 
-Write-Host "Time's up!"
+Write-Host "Time's up!"		#Could be the end, but write a Complete_Flag_Temp File Somewhere to scan for...
+New-Item -Path "C:/Users/RFacella/Documents/Dev/powershell_complete.flag" -ItemType File -Force
